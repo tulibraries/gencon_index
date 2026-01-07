da908daf3832e9cd056ed3b0bef6ceacc6f794c9
Steven Ng <steven.ng@temple.edu>
2026-01-07 09:38:30 -0700

    Autocorrect lint problems

--- harvest_all.rb
+++ harvest_all.rb
@@ -1,21 +1,22 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'dotenv/load'
-require_relative "lib/harvest_csv"
+require_relative 'lib/harvest_csv'
 
 Dotenv.load
 
-files = Dir["/tmp/gencon/*.csv"]
+files = Dir['/tmp/gencon/*.csv']
 files.sort.each do |fn|
   fp = File.expand_path(fn)
   puts "process #{fp}"
   command = [
-    "./csv2solr",
-    "harvest",
+    './csv2solr',
+    'harvest',
     "'#{fp}'",
-    "--mapfile=./solr_map.yml",
-    "--solr-url=#{ENV['SOLR_URL']}"].join(" ")
+    '--mapfile=./solr_map.yml',
+    "--solr-url=#{ENV['SOLR_URL']}"
+  ].join(' ')
   puts command
   `#{command}`
 end
-
